04a2bcb45dbfe3a43a3242d9ea310df425433cdc
arock1998 <arock1998@naver.com>
2023-10-10 14:30:31 +0900

    feat: signup 기능 연결

--- frontend/pi5neer_fe/src/pages/auth/SignUp.js
+++ frontend/pi5neer_fe/src/pages/auth/SignUp.js
@@ -1,6 +1,38 @@
 import styles from './Auth.module.css';
+import { useState } from "react";
+import axios from "axios";
 
 function SignUp() {
+
+    const [cName, setCName] = useState("");
+    const [email, setEmail] = useState("");
+    const [pw, setPW] = useState("");
+    const [doAddr, setDoAddr] = useState("");
+    const [ipAddr, setIpAddr] = useState("");
+
+    const signUpAxios = async (e) => {
+        e.preventDefault();
+
+        await axios.post("/users/signup", {
+            companyName : cName,
+            email : email,
+            password : pw,
+            domain_address : doAddr,
+            IP_address : ipAddr,
+            membership : 'Basic'
+          })
+          .then((response) => {
+            if ((response.status = 200)) {
+                alert("회원가입 되셨습니다. 로그인 해 주세요");
+              return navigate("/signin");
+            }
+          })
+          .catch((err) => {
+            alert("에러가 발생했습니다. 관리자에게 문의해주세요" + err);
+          });
+      };
+
+
     return (
         <div className={styles.Screen}>
             <div className={styles.LogoWindow}>
@@ -11,14 +43,13 @@ function SignUp() {
                 <div className={styles.ShadowBox}>
                     <div className={styles.InputPadding}>
                         <h2>Sign up for a free account</h2>
-                        <input className={styles.InputField} placeholder="Input Company Name"></input>
-                        <input className={styles.InputField} placeholder="Input Password" type="password"></input>
-                        <input className={styles.InputField} placeholder="Email address"></input>
-                        <input className={styles.InputField} placeholder="Create password"></input>
-                        <input className={styles.InputField} placeholder="Domain address"></input>
-                        <input className={styles.InputField} placeholder="IP address"></input>
-                        <button className={styles.LoginBtn} type="button">Register</button>
-                        <div className={styles.NavigateBtn}>
+                        <input className={styles.InputField} onChange={(e) => setCName(e.target.value)} placeholder="Input Company Name"></input>
+                        <input className={styles.InputField} onChange={(e) => setEmail(e.target.value)} id="email" placeholder="Email address"></input>
+                        <input className={styles.InputField} onChange={(e) => setPW(e.target.value)}  id="pw" placeholder="Input Password" type="password"></input>
+                        <input className={styles.InputField} onChange={(e) => setDoAddr(e.target.value)} id="doAddr" placeholder="Domain address"></input>
+                        <input className={styles.InputField} onChange={(e) => setIpAddr(e.target.value)} id="ipAddr"placeholder="IP address"></input>
+                        <button className={styles.LoginBtn} onClick={signUpAxios} type="button">Register</button>
+                        <div className={styles.NavigateBtn} >
                             <a href="./">sign in</a>
                         </div>
                     </div>
