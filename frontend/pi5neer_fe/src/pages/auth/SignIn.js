689edb21ef082603fbbc799c097ad719be2d6f5c
arock1998 <arock1998@naver.com>
2023-10-10 15:15:47 +0900

    feat: signin 기능 연결 및 정규표현식 추가

--- frontend/pi5neer_fe/src/pages/auth/SignIn.js
+++ frontend/pi5neer_fe/src/pages/auth/SignIn.js
@@ -1,6 +1,45 @@
 import styles from './Auth.module.css';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+import axios from "axios";
 
 function SignIn() {
+
+    const navigate = useNavigate();
+
+    const [cName, setCName] = useState("");
+    const [pw, setPW] = useState("");
+
+    const signInAxios = async (e) => {
+        e.preventDefault();
+        
+        const cn_reg = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/;
+        const pw_reg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+        if( !cn_reg.test(cName) ) {
+            alert("회사 이름을 확인해주세요.");
+            return;
+        }
+        if( !pw_reg.test(pw) ) {
+            alert("비밀번호를 확인해주세요.");
+            return;
+        }
+
+        await axios.post("/users/login", {
+            companyName : cName,
+            password : pw,
+          })
+          .then((response) => {
+            if (response.status = 200) {
+              return navigate("/main");
+            } else if (response.status = 401) {
+                alert("다시 확인해주세요");
+            }
+          })
+          .catch((err) => {
+            alert("에러가 발생했습니다. 관리자에게 문의해주세요\n" + err);
+          });
+      };
+
     return (
         <div className={styles.Screen}>
             <div className={styles.LogoWindow}>
@@ -11,9 +50,11 @@ function SignIn() {
                 <div className={styles.ShadowBox}>
                     <div className={styles.InputPadding}>
                         <h2>Sign in</h2>
-                        <input className={styles.InputField} placeholder="Input Company Name"></input>
-                        <input className={styles.InputField} placeholder="Input Password" type="password"></input>
-                        <button className={styles.LoginBtn} type="button">Login</button>
+                        <form onSubmit={signInAxios}> 
+                            <input className={styles.InputField} onChange={(e) => setCName(e.target.value)} placeholder="Input Company Name"></input>
+                            <input className={styles.InputField} onChange={(e) => setPW(e.target.value)} placeholder="Input Password" type="password"></input>
+                            <button className={styles.LoginBtn} type="submit">Login</button>
+                        </form>
                         <div className={styles.NavigateBtn}>
                             <a href="./signup">sign up</a>
                             <div>&nbsp;/&nbsp;</div>
