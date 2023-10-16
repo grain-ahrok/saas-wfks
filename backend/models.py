cf04fd1840a75a0a40f966e4e62743e370dbda89
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-10-16 17:42:00 +0900

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -11,3 +11,8 @@ class User(db.Model):
     domain_address = db.Column(db.String(255))
     IP_address = db.Column(db.String(15))
     membership = db.Column(db.String(20))
+    
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
