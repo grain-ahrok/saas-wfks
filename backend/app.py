75710cb2c4985c785bbe5acaf98fde2c8adb9034
arock1998 <arock1998@naver.com>
2023-10-17 16:35:30 +0900

    feat: DB 생성 시기 변경

--- backend/app.py
+++ backend/app.py
@@ -3,16 +3,20 @@ from flask import Flask
 from config import Config
 from models import db
 from routes.users import app as users  # Changed the import
+from routes.admin import app as admin
 from flask_cors import CORS
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 app.register_blueprint(users)  # Registering the blueprint
+app.register_blueprint(admin)
 
 # Configure CORS
 CORS(app, origins='http://localhost:3000', supports_credentials=True)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,5 @@
 # config.py
 class Config:
-    SQLALCHEMY_DATABASE_URI_USERS = 'sqlite:///users.db'
-    SQLALCHEMY_DATABASE_URI_ADMIN = 'sqlite:///admin.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///pi5neer.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'Pi5neer123!@#'
