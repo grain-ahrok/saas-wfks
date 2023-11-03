cb461b17198bf5471f0511ac6f0c2fbadfde2766
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-11-03 16:09:55 +0900

    Update admin.py

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, redirect, url_for
 from models import Admin
 from utils import bcrypt
 
@@ -13,7 +13,17 @@ def admin_login():
 
     admin = Admin.query.filter_by(username=username).first()
 
-    if admin and bcrypt.check_password_hash(admin.password, password):
-        return jsonify({"message": "Login successful."}), 200
+    if admin:
+        if bcrypt.check_password_hash(admin.password, password):
+            # 로그인에 성공했을 때 필요하면 로직 구현
+            # 관리자 페이지로 리다이렉션합니다.
+            return redirect(url_for('admin.admin_page'))
+        else:
+            return jsonify({"message": "Login failed. Invalid credentials."}), 401
     else:
-        return jsonify({"message": "Login failed. Invalid credentials."}), 401
+        return jsonify({"message": "Login failed. User not found."}), 401
+
+@app.route('/admin_page', methods=['GET'])
+def admin_page():
+    # 관리자 페이지에 대한 로직을 여기에 추가
+    return "Welcome to the admin page!"
