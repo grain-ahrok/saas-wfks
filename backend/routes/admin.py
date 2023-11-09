61b7010d88cbfbadf89303255b8b51e88823a920
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-11-09 15:46:25 +0900

    Update admin.py

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -2,10 +2,10 @@ from flask import Blueprint, request, jsonify, redirect, url_for
 from models import Admin
 from utils import bcrypt
 
-admin = Blueprint('admin', __name__, url_prefix='/admin')
+Pi5neer = Blueprint('admin', __name__, url_prefix='/Pi5neer')
 
 # 관리자 로그인 API
-@admin.route('/login', methods=['POST'])
+@Pi5neer.route('/signin', methods=['POST'])
 def admin_login():
     data = request.get_json()
     username = data.get('username')
@@ -18,13 +18,19 @@ def admin_login():
         if bcrypt.check_password_hash(admin.password, password):
             # 로그인에 성공했을 때 필요하면 로직 구현
             # 관리자 페이지로 리다이렉션합니다.
-            return redirect(url_for('admin.admin_page'))
+            return redirect(url_for('admin.admin_dashboard')), 200
         else:
             return jsonify({"message": "Login failed. Invalid credentials."}), 401
     else:
         return jsonify({"message": "Login failed. User not found."}), 401
 
-@admin.route('/admin_page', methods=['GET'])
+@Pi5neer.route('/dashboard', methods=['GET'])
 def admin_page():
     # 관리자 페이지에 대한 로직을 여기에 추가
-    return "Welcome to the admin page!"
+    return "Welcome to the admin page!", 200
+
+
+@Pi5neer.route('/user-management', methods=['GET'])
+def user_mgmt():
+    #고객사 전체적인 관리 페이지에 대한 특성 로직들 여기에 추가
+    return "This page is user-management page!", 200
