92a71d82f3325a7514041f179a1f423ef50fa43f
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-11-09 15:55:16 +0900

    Update app.py
    Pi5neer가 admin 페이지 tree

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from config import Config
 from models import db, bcrypt
 from routes.users import users
 from routes.customer import customer
-from routes.admin import admin
+from routes.admin import Pi5neer
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ bcrypt.init_app(app)
 
 app.register_blueprint(users)
 app.register_blueprint(customer)
-app.register_blueprint(admin)
+app.register_blueprint(Pi5neer)
 
 # Configure CORS    
 CORS(app, origins='http://localhost:3000', supports_credentials=True)
