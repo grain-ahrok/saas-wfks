bb89d687080ee7a08331ebc02cb2f9afa59e4bd9
shw7007 <shw885547@gmail.com>
2023-11-10 17:17:05 +0900

    Ipexception list made

--- frontend/pi5neer_fe/src/pages/securitySetting/ExceptionIpPage.tsx
+++ frontend/pi5neer_fe/src/pages/securitySetting/ExceptionIpPage.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
 
 type Props = {}
 
 const ExceptionIpPage = (props: Props) => {
   return (
-    <div>ExceptionIpPage</div>
-  )
+    <Box
+      sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
+    >
+      <FixedSizeList
+        height={400}
+        width={360}
+        itemSize={46}
+        itemCount={200}
+        overscanCount={5}
+      >
+        {renderRow}
+      </FixedSizeList>
+    </Box>
+  );
 }
+
+function renderRow(props: ListChildComponentProps) {
+  const { index, style } = props;
+
+  return (
+    <ListItem style={style} key={index} component="div" disablePadding>
+      <ListItemButton>
+        <ListItemText primary={`Item ${index + 1}`} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
 
 export default ExceptionIpPage
