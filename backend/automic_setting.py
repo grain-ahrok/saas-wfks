2a9bb32e8603cf8c1332ced2afc79adbcba76e99
arock1998 <arock1998@naver.com>
2023-11-27 05:21:26 +0900

    fix: 보안정책 복사 안되는 문제 해결

--- backend/automic_setting.py
+++ backend/automic_setting.py
@@ -122,7 +122,7 @@ def get_existing_priorities(url, headers):
 def create_security_policy(data, token):
     security_policy_url = "https://wf.awstest.piolink.net:8443/api/v3/security_policy"
     headers = {'Authorization': 'token ' + token}
-    post_data = {"name": data.get('companyName'),"status": "enable","create_type": "vendor_policy","vendor_policy": "standard","vendor_policy_block_status": "disable","sync_id": 4}
+    post_data = {"name": data.get('companyName'),"status": "enable","create_type": "sync","vendor_policy": "standard", "vendor_policy_block_status": "disable","sync_id": 4}
     response = make_api_request(security_policy_url, "POST", headers, post_data)
     if response:
         if response.status_code == 200:
